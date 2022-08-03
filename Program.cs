c442eef68340063834f0d201b53fbd7fd9ff89d6
yogeshreeturkar111 <110226741+yogeshreeturkar111@users.noreply.github.com>
2022-08-03 22:31:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Power
+{
+    public class PowerOf2
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int no;
+                int remainder;
+                Console.WriteLine("Enter a no to check");
+                no = Convert.ToInt32(Console.ReadLine());
+                if (no == 1)
+                    Console.Write("It is power of 2.");
+                else
+                {
+                    while (no > 1)
+                    {
+                        remainder = no % 2;
+                        if (remainder != 0)
+                            break;
+                        no /= 2;
+                    }
+                    if (no == 1)
+                        Console.WriteLine("It is a power of 2");
+                    else
+                        Console.WriteLine("It is not a power of 2");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
